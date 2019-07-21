25af99e38ee3cc247bf2e34a1d34ee137b8c4899
Victor Johnsson <victor.johnsson@gmail.com>
2019-07-21 09:50:22 +0200

    Added player for which position is updated

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,21 @@ module Main where
 
 import           SDL
 import           Control.Concurrent             ( threadDelay )
+import           Model
 
 main :: IO ()
 main = do
     initialize [InitJoystick]
     joysticks <- availableJoysticks
     mapM_ openJoystick joysticks
-    printEvents
+    gameLoop initModel
 
-printEvents :: IO ()
-printEvents = do
+gameLoop :: Model -> IO ()
+gameLoop m = do
     threadDelay 15000
     events <- pollEvents
-    mapM_ print events
-    printEvents
+    --mapM_ print events
+    msSinceSdlLibInit <- ticks
+    let newM = updateModel m events msSinceSdlLibInit
+    print newM
+    gameLoop newM
