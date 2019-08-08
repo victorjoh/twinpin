e8b52d25ee0481fb40755609ed05246fa5224da0
Victor Johnsson <victor.johnsson@gmail.com>
2019-08-08 22:04:18 +0200

    reduced jaggedness by increasing bmp size
    The player visual gets jagged when rotating.

--- Setup.hs
+++ Setup.hs
@@ -6,19 +6,19 @@ import           Graphics.Rasterific.Svg        ( loadCreateFontCache
                                                 )
 import           System.Directory               ( createDirectoryIfMissing )
 
-loadRender :: FilePath -> FilePath -> IO ()
-loadRender svgfilename bmpfilename = do
-    f <- loadSvgFile svgfilename
+loadRender :: FilePath -> FilePath -> (Int, Int) -> IO ()
+loadRender svgPath bmpPath bmpSize = do
+    f <- loadSvgFile svgPath
     case f of
         Nothing  -> putStrLn "Error while loading SVG"
         Just doc -> do
             cache           <- loadCreateFontCache "gen/fonty-texture-cache"
-            (finalImage, _) <- renderSvgDocument cache Nothing 96 doc
-            writeBitmap bmpfilename finalImage
+            (finalImage, _) <- renderSvgDocument cache (Just bmpSize) 96 doc
+            writeBitmap bmpPath finalImage
 
 main :: IO ()
 main = do
     createDirectoryIfMissing False "gen"
-    loadRender "images/player.svg" "gen/player.bmp"
+    loadRender "images/player.svg" "gen/player.bmp" (128, 128)
     defaultMain
 
